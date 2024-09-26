f6af0e77c2d4b72be2e5c161335048f4b4008b06
Joseph Anderson <joseph_anderson@fitnyc.edu>
2024-09-26 10:17:24 -0400

    Initial commit

---
+++ asset/js/theme.js
@@ -0,0 +1,151 @@
+$(document).ready(function () {
+  // Item browse and search
+  if ($('body').hasClass('resource browse') || $('body').hasClass('resource search')) {
+    // Infinite scroll/load more
+    // check if results have more than one page
+    if ($('.pagination .next').length) {
+      var status = $(`
+        <div class="page-load-status">
+          <div class="row justify-content-center pb-5 mb-5">
+            <div class="col-auto">
+              <div class="spinner-border infinite-scroll-request" role="status">
+                <span class="visually-hidden">Loading...</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        `);
+      status.hide();
+      var loadButton = `
+      <div class="row justify-content-center">
+        <div class="col-auto">
+          <button id="load-button" class="btn btn-fit-green floating-action" type="button" aria-controls="browse-container" aria-label="Load more results">
+            <span class="action-container">
+              <i class="fas fa-plus" aria-hidden="true" title="Load more results">
+              </i>
+              Load More
+            </span>
+          </button>
+        </div>
+      </div>
+      `;
+      $('.pagination-row').after(loadButton).after(status);
+      $('#browse-container').attr('aria-live', 'polite')
+      let nextURL;
+      let appendElement = '.item-container';
+      let currentItemCount;
+
+      function updateNextURL(doc) {
+        if ($(doc).find('.next').length) {
+          nextURL = $(doc).find('.next').attr('href');
+          if (!nextURL.includes('&scroll_request=true')) {
+            nextURL = nextURL + '&scroll_request=true';
+          }
+        } else {
+          nextURL = null;
+        }
+      }
+
+      function getLinkData(doc) {
+        if ($(doc).find('script[type="application/ld+json"]').length) {
+          var newLinkData = $(doc).find('script[type="application/ld+json"]');
+          $('script[type="application/ld+json"]').last().after(newLinkData);
+        }
+      }
+
+      function updateFocus() {
+        var firstLoadedElement = $(appendElement)[currentItemCount];
+        $(firstLoadedElement).find('a.card-img').focus();
+        getItemCount();
+      }
+
+      function getItemCount() {
+        currentItemCount = $(appendElement).length
+      }
+      // get initial nextURL
+      updateNextURL(document);
+      getItemCount();
+      let $container = $('#browse-container').infiniteScroll({
+        // options
+        // use function to set custom URLs
+        path: function () {
+          return nextURL;
+        },
+        checkLastPage: '.next',
+        append: appendElement,
+        scrollThreshold: false,
+        button: '#load-button',
+        hideNav: '.pagination-row',
+        status: '.page-load-status',
+        history: false,
+      });
+      // update nextURL on page load
+      $container.on('load.infiniteScroll', function (event, body, path, response) {
+        history.replaceState(null, null, path.replace('&scroll_request=true', ''));
+        updateNextURL(body);
+        getLinkData(body);
+      });
+      $container.on('append.infiniteScroll', function () {
+        updateFocus();
+      });
+    }
+    document.querySelectorAll('.facet-select').forEach((el) => {
+      let settings = {
+        plugins: ['remove_button'],
+        hidePlaceholder: true,
+        closeAfterSelect: true,
+        allowEmptyOption: true,
+        maxOptions: null,
+        sortField: {
+          field: "text",
+          direction: "asc"
+        },
+        onItemAdd: function (value) {
+          window.location.href = value;
+        },
+        onItemRemove: function (value) {
+          window.location.href = value;
+        },
+        onInitialize: function () {
+          el.style.visibility = "visible";
+
+        },
+        render: {
+          option: function (data, escape) {
+
+            return `<div>${escape(data.text)}${data.count ? ' (' + escape(data.count) + ')' : ''}</div>`;
+          }
+        }
+      };
+      new TomSelect(el, settings);
+    });
+    $(".file-slide-option").change(function () {
+      $(".file-slide-option").not(this).prop('checked', false);
+    });
+  }
+  //Media viewer
+  if ($('body').hasClass('show')) {
+    //clipboard
+    $('.clip-button').each(function (index) {
+      new ClipboardJS(this);
+    });
+    $("#text-download").on("click.fashionCalendar", function (event) {
+      event.preventDefault();
+      saveAs($(this).attr("href"), $(this).data("id") + ".txt");
+    });
+    $("#pdf-download").on("click.fashionCalendar", function (event) {
+      const id = $(this).data("id")
+      fetch(`/data-atlas-api/download?id=${id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.url);
+          window.location.href = data.url;
+        }).catch((error) => {
+          console.log(error);
+          console.log(`Error downloading PDF`);
+        });
+    });
+    let title = $("#quick-details .title").text();
+    $(".pdf-container iframe").attr("title", title);
+  }
+});
---
+++ helper/CitationHelper.php
@@ -0,0 +1,111 @@
+<?php
+namespace OmekaTheme\Helper;
+
+use Laminas\View\Helper\AbstractHelper;
+use Omeka\Api\Representation\ItemRepresentation;
+
+class CitationHelper extends AbstractHelper
+{
+  public function __invoke(ItemRepresentation $item)
+  {
+    $escape = $this->getView()->plugin('escapeHtml');
+    $title = $item->displayTitle();
+    $url = $escape($item->url());
+    $apa = 'Finley, R. <em>' . $title . '</em>. Fashion Calendar Research Database, ' . $url;
+    $chicago = 'Finley, Ruth. "' . $title . '". Fashion Calendar Research Database. ' . $url;
+    $mla = 'Finley, Ruth. <em>' . $title . '</em>. Fashion Calendar Research Database, ' . $url;
+    return '
+        <ul class="nav nav-tabs mb-3" id="citationTab" role="tablist">
+          <li class="nav-item" role="presentation">
+            <button class="nav-link active text-light" id="apa-tab" data-bs-toggle="tab" data-bs-target="#apa" type="button" role="tab" aria-controls="apa" aria-selected="true">APA</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link text-light" id="mla-tab" data-bs-toggle="tab" data-bs-target="#mla" type="button" role="tab" aria-controls="mla" aria-selected="false">MLA</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link text-light" id="chicago-tab" data-bs-toggle="tab" data-bs-target="#chicago" type="button" role="tab" aria-controls="chicago" aria-selected="false">Chicago/Turabian</button>
+          </li>
+        </ul>
+        <div class="tab-content" id="citationTabContent">
+          <div class="tab-pane fade show active" id="apa" role="tabpanel" aria-labelledby="apa-tab">
+            <div class="input-group mb-3">
+              <div class="form-control font-monospace text-break" id="apaCitation">
+              ' . str_replace('..', '.', $apa) . '
+              </div>
+              <button class="btn btn-dark clip-button" type="button" id="apa-button" data-clipboard-target="#apaCitation" aria-label="Copy citation to clipboard">
+                <i class="fas fa-copy" title="Copy citation to clipboard" aria-hidden="true"></i>
+              </button>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="mla" role="tabpanel" aria-labelledby="mla-tab">
+          <div class="input-group mb-3">
+            <div class="form-control font-monospace text-break" id="mlaCitation">
+            ' . str_replace('..', '.', $mla) . '
+            </div>
+            <button class="btn btn-dark clip-button" type="button" id="mla-button" data-clipboard-target="#mlaCitation" aria-label="Copy citation to clipboard">
+              <i class="fas fa-copy" title="Copy citation to clipboard" aria-hidden="true"></i>
+            </button>
+          </div>
+          </div>
+          <div class="tab-pane fade" id="chicago" role="tabpanel" aria-labelledby="chicago-tab">
+          <div class="input-group mb-3">
+            <div class="form-control font-monospace text-break" id="chicagoCitation">
+            ' . str_replace('..', '.', $chicago) . '
+            </div>
+            <button class="btn btn-dark clip-button" type="button" id="chicago-button" data-clipboard-target="#chicagoCitation" aria-label="Copy citation to clipboard">
+              <i class="fas fa-copy" title="Copy citation to clipboard" aria-hidden="true"></i>
+            </button>
+          </div>
+          </div>
+        </div>
+
+        ';
+  }
+  // Converts $title to Title Case, and returns the result.
+  protected function titleCase($title)
+  {
+    $smallwordsarray = array(
+      'of',
+      'a',
+      'the',
+      'and',
+      'an',
+      'or',
+      'nor',
+      'but',
+      'is',
+      'if',
+      'then',
+      'else',
+      'when',
+      'at',
+      'from',
+      'by',
+      'on',
+      'off',
+      'for',
+      'in',
+      'out',
+      'over',
+      'to',
+      'into',
+      'with'
+    );
+
+    // Split the string into separate words
+    $words = explode(' ', $title);
+
+    foreach ($words as $key => $word) {
+      // If this word is the first, or it's not one of our small words, capitalise it
+      // with ucwords().
+      if ($key == 0 or !in_array($word, $smallwordsarray)) {
+        $words[$key] = ucwords($word);
+      }
+    }
+
+    // Join the words back into a string
+    $newtitle = implode(' ', $words);
+
+    return $newtitle;
+  }
+}
